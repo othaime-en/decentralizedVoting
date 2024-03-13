cb1cb3400bf333befca87333d860680966e91675
othaime-en <61498231+othaime-en@users.noreply.github.com>
2024-03-13 07:31:00 -0500

    added pagination

--- client/src/components/DataTable.jsx
+++ client/src/components/DataTable.jsx
@@ -1,14 +1,14 @@
 // DataTable.js
 import React, { useState } from "react";
-import { useTable, useSortBy, useFilters } from "react-table";
+import { useTable, useSortBy, useFilters, usePagination } from "react-table";
 
 import { getStatusColor } from "../utils";
 
 const DataTable = ({ columns, data }) => {
-  // State to keep track of filter inputs
   const [titleFilter, setTitleFilter] = useState("");
   const [statusFilter, setStatusFilter] = useState("");
 
+  // Define enhanced columns with color-coded statuses
   const enhancedColumns = React.useMemo(() => {
     return columns.map((col) => {
       if (col.accessor === "instanceStatus") {
@@ -16,7 +16,7 @@ const DataTable = ({ columns, data }) => {
           ...col,
           Cell: ({ value }) => {
             const color = getStatusColor(value);
-            return <span style={{ color }}>{value}</span>;
+            return <div style={{ color }}>{value}</div>;
           },
         };
       }
@@ -24,17 +24,36 @@ const DataTable = ({ columns, data }) => {
     });
   }, [columns]);
 
+  // Setup for useTable including sorting, filtering, and pagination
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    page, // Use 'page', not 'rows'. 'rows' would be the entire dataset.
     prepareRow,
     setFilter,
     setAllFilters,
-  } = useTable({ columns: enhancedColumns, data }, useFilters, useSortBy);
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns: enhancedColumns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 10 }, // Set the initial page index and size
+    },
+    useFilters,
+    useSortBy,
+    usePagination
+  );
 
-  // Resets all filters
+  // Function to reset all filters
   const resetFilters = () => {
     setTitleFilter("");
     setStatusFilter("");
@@ -96,25 +115,75 @@ const DataTable = ({ columns, data }) => {
             {...getTableBodyProps()}
             className="bg-gray-800 divide-y divide-gray-700"
           >
-            {rows.map((row) => {
+            {page.map((row) => {
               prepareRow(row);
               return (
                 <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return (
-                      <td
-                        {...cell.getCellProps()}
-                        className="px-4 py-3 text-sm text-gray-400"
-                      >
-                        {cell.render("Cell")}
-                      </td>
-                    );
-                  })}
+                  {row.cells.map((cell) => (
+                    <td
+                      {...cell.getCellProps()}
+                      className="px-4 py-3 text-sm text-gray-400"
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  ))}
                 </tr>
               );
             })}
           </tbody>
         </table>
+      </div>
+      {/* Pagination control */}
+      <div className="flex justify-between items-center py-3 bg-[#1c1c24] text-white">
+        <div className="flex items-center gap-2">
+          <button
+            onClick={() => gotoPage(0)}
+            disabled={!canPreviousPage}
+            className="text-xs text-white bg-gray-700 hover:bg-gray-600 disabled:bg-gray-700 px-2 py-1 rounded-md"
+          >
+            {"<<"}
+          </button>
+          <button
+            onClick={() => previousPage()}
+            disabled={!canPreviousPage}
+            className="text-xs text-white bg-gray-700 hover:bg-gray-600 disabled:bg-gray-700 px-2 py-1 rounded-md"
+          >
+            {"<"}
+          </button>
+          <button
+            onClick={() => nextPage()}
+            disabled={!canNextPage}
+            className="text-xs text-white bg-gray-700 hover:bg-gray-600 disabled:bg-gray-700 px-2 py-1 rounded-md"
+          >
+            {">"}
+          </button>
+          <button
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+            className="text-xs text-white bg-gray-700 hover:bg-gray-600 disabled:bg-gray-700 px-2 py-1 rounded-md"
+          >
+            {">>"}
+          </button>
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="text-xs">
+            Page{" "}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{" "}
+          </span>
+          <select
+            value={pageSize}
+            onChange={(e) => setPageSize(Number(e.target.value))}
+            className="text-xs text-gray-300 bg-gray-800 border border-gray-600 rounded-md p-1"
+          >
+            {[5, 10, 20, 30].map((size) => (
+              <option key={size} value={size}>
+                Show {size}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     </>
   );
