d8fdd0e6d4c09855604330016087dc9ba69fee84
othaime-en <61498231+othaime-en@users.noreply.github.com>
2024-03-18 01:37:36 -0500

    fixed minor issues

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import { DisplayCampaigns } from '../components';
-import { useStateContext } from '../context'
+import { DisplayCampaigns } from "../components";
+import { useStateContext } from "../context";
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -14,19 +14,19 @@ const Home = () => {
     const data = await getAllInstances();
     setCampaigns(data);
     setIsLoading(false);
-  }
+  };
 
   useEffect(() => {
-    if(contract) fetchCampaigns();
+    if (contract) fetchCampaigns();
   }, [address, contract]);
 
   return (
-    <DisplayCampaigns 
+    <DisplayCampaigns
       title="All Instances"
       isLoading={isLoading}
       campaigns={instances}
     />
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
