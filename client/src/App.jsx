66fa33ecbcdb88c226dc7de52aadb3100f07d3f2
othaime-en <61498231+othaime-en@users.noreply.github.com>
2025-05-20 17:21:24 -0400

    feat: Implement dark/light theme and responsive 4-column card grid with improved layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import { useTheme } from "./context/ThemeContext";
 
 import { Sidebar, Navbar } from "./components";
 import {
@@ -11,29 +13,42 @@ import {
   InstanceConfigDetails,
 } from "./pages";
 
-const App = () => {
+const AppContent = () => {
+  const { isDarkMode } = useTheme();
+  
   return (
-    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-      <div className="sm:flex hidden mr-10 relative">
+    <div className={`relative min-h-screen flex flex-row transition-colors duration-300 ${
+      isDarkMode ? 'bg-[#13131a]' : 'bg-gray-100'
+    }`}>
+      <div className="sm:flex hidden relative">
         <Sidebar />
       </div>
 
-      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+      <div className="flex-1 flex flex-col transition-all duration-300 ease-in-out">
         <Navbar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-instance" element={<CreateInstance />} />
-          <Route path="/instance-details/:id" element={<CampaignDetails />} />
-          <Route
-            path="/instance-config/:id"
-            element={<InstanceConfigDetails />}
-          />
-          <Route path="/analytics" element={<Analytics />} />
-        </Routes>
+        <div className="px-4 sm:px-6 lg:px-8 py-4 w-full max-w-[2000px] mx-auto">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-instance" element={<CreateInstance />} />
+            <Route path="/instance-details/:id" element={<CampaignDetails />} />
+            <Route
+              path="/instance-config/:id"
+              element={<InstanceConfigDetails />}
+            />
+            <Route path="/analytics" element={<Analytics />} />
+          </Routes>
+        </div>
       </div>
     </div>
+  );
+};
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
   );
 };
 
